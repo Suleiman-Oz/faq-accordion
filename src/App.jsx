e330f78d9860ac73cafa96b2d809a304855c8565
Suleiman <sulemancusman@gmail.com>
2025-08-24 09:30:16 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import './style.css'
 import Accordion from './components/Accordion'
 
 function App() {
 
   return (
-    <div>
+    <div className="container">
       <Accordion />
     </div>
   )
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React ,{ useState }from 'react'
+import data from '../data'
+import iconMinus from "../assets/images/icon-minus.svg"
+import iconPlus from "../assets/images/icon-plus.svg"
+import iconStar from "../assets/images/icon-star.svg"
+import "../style.css"
 
 const Accordion = () => {
+  const [selected, setSelected] = useState(null)
+  console.log(iconMinus)
+
+  const handleSingle = (id) => {
+    setSelected(id === selected ? null : id)
+  }
   return (
-    <div>
-      <h1>FAQs</h1>
+    <div className="container-accordion">
+      <div className="accordion">
+        <div className="card-accordion">
+          <div className="header">
+            <img src={iconStar} alt="star icon" />
+            <span>FAQs</span>
+          </div>
+          {data.length > 0 &&
+            data.map((item) => (
+              <div className="item" key={item.id}>
+                <div className="title" onClick={() => handleSingle(item.id)}>
+                  <h3>{item.question}</h3>
+                  <span>
+                    {item.id === selected ? (
+                      <img src={iconMinus} alt="minus icon" />
+                    ) : (
+                      <img src={iconPlus} alt="minus icon" />
+                    )}
+                  </span>
+                </div>
+
+                {selected === item.id ? (
+                  <div className="content">{item.answer}</div>
+                ) : null}
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
